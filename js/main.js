7311988ea458887539cce11076e83c935016cbbd
MBacioglu <302145958@student.rocmondriaan.nl>
2024-02-02 15:14:30 +0100

    OPR1

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const temperatures = [
+    //1a
+{
+    location: "Den Haag",
+    temp: 15,
+},
+
+{
+    location: "Rotterdam",
+    temp: 18,
+},
+
+{
+    location: "Eindhoven",
+    temp: 17,
+}
+
+{
+    location: "Amsterdam",
+    temp: 16,
+}
+
+]
+
+//1b
+createTable(temperatures);
+
+//2a
+const objData = JSON.parse(jsonData)
+for (let index = 0; index < objData.length; index++) {
+    const object = objData[index];
+    
+}
